93ee75867e465153a27042d29e608dc6cb72cb4a
Kimon Meier <kimon.meier@gmail.com>
2025-03-09 13:44:54 +0100

    Fixed an error with the implementation of how the logging is used and injected.

---
+++ BaseChord.Api/Logging/LoggingServiceFactory.cs
@@ -0,0 +1,26 @@
+﻿using CorrelationId.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace BaseChord.Api.Logging
+{
+    public static class LoggingServiceFactory
+    {
+        public static void AddCustomizedLogging(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((context, config) =>
+                {
+                    config
+                        .Enrich.FromLogContext()
+                        .MinimumLevel.ControlledBy(new LoggingLevelSwitch(LogEventLevel.Information))
+                        .MinimumLevel.Override("CorrelationId", LogEventLevel.Error)
+                        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                        .WriteTo.File(Path.Combine(Environment.CurrentDirectory, "logs", "chorconnect.log"), rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+                });
+        }
+    }
+}
