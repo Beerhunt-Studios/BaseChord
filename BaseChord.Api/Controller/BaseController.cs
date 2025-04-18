a5d8d43c2f909464cbd0a80c967472e0f9cb2d6b
Kimon Meier <kimon.meier@gmail.com>
2025-04-18 12:58:29 +0200

    Added a method in the controller to detect the external identifier of the user

--- BaseChord.Api/Controller/BaseController.cs
+++ BaseChord.Api/Controller/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BaseChord.Api.Controller;
 
@@ -8,4 +9,13 @@ public abstract class BaseController : ControllerBase
     protected ISender Sender { get; init; }
 
     public BaseController(ISender sender) => Sender = sender;
+
+    /// <summary>
+    /// Searches the httpcontext for the external identifier provided by auth0
+    /// </summary>
+    /// <returns>The external identifier provided via an JWT</returns>
+    public string? GetExternalIdentifier()
+    {
+        return (HttpContext.User.Identity as ClaimsIdentity)?.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+    }
 }
