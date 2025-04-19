987e79854673df0b196c54997c3582994ee189b4
Kimon Meier <kimon.meier@gmail.com>
2025-04-19 19:08:24 +0200

    Added the ValueObject together with its automatic configuration

---
+++ BaseChord.Domain/ValueObject.cs
@@ -0,0 +1,84 @@
+﻿namespace BaseChord.Domain;
+
+/// <summary>
+/// Represents a base class for value objects, implementing equality based on atomic values.
+/// Inherits from <see cref="IEquatable{T}"/> and provides structural equality comparison.
+/// Compatible with Entity Framework Core's owned types pattern.
+/// </summary>
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    /// <summary>
+    /// Provides the atomic values of the value object that are used for equality comparison.
+    /// Derived classes must override this method and yield return each component that should be used in equality and hash code calculations.
+    /// </summary>
+    /// <returns>An enumerable of atomic values used for equality.</returns>
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    /// <summary>
+    /// Determines whether the specified object is equal to the current value object.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current value object.</param>
+    /// <returns><c>true</c> if the specified object is equal to the current value object; otherwise, <c>false</c>.</returns>
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals(obj as ValueObject);
+    }
+
+    /// <summary>
+    /// Determines whether the specified value object is equal to the current value object.
+    /// </summary>
+    /// <param name="other">The value object to compare with the current value object.</param>
+    /// <returns><c>true</c> if the specified value object is equal to the current value object; otherwise, <c>false</c>.</returns>
+    public bool Equals(ValueObject? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    /// <summary>
+    /// Returns a hash code for the value object, based on its equality components.
+    /// </summary>
+    /// <returns>A hash code for the current value object.</returns>
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(1, (hash, obj) => HashCode.Combine(hash, obj));
+    }
+
+    /// <summary>
+    /// Determines whether two value objects are equal.
+    /// </summary>
+    /// <param name="left">The first value object to compare.</param>
+    /// <param name="right">The second value object to compare.</param>
+    /// <returns><c>true</c> if the value objects are equal; otherwise, <c>false</c>.</returns>
+    public static bool operator ==(ValueObject? left, ValueObject? right) =>
+        Equals(left, right);
+
+    /// <summary>
+    /// Determines whether two value objects are not equal.
+    /// </summary>
+    /// <param name="left">The first value object to compare.</param>
+    /// <param name="right">The second value object to compare.</param>
+    /// <returns><c>true</c> if the value objects are not equal; otherwise, <c>false</c>.</returns>
+    public static bool operator !=(ValueObject? left, ValueObject? right) =>
+        !Equals(left, right);
+}
--- BaseChord.Infrastructure/Database/ModelBuilderExtensions.cs
+++ BaseChord.Infrastructure/Database/ModelBuilderExtensions.cs
@@ -1,23 +1,83 @@
 ﻿using MassTransit;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using BaseChord.Domain;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace BaseChord.Infrastructure.Database;
-
-public static class ModelBuilderExtensions
+namespace BaseChord.Infrastructure.Database
 {
-    public static void AddInfrastructure(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+    /// <summary>
+    /// Provides extension methods for <see cref="ModelBuilder"/> to configure the database schema.
+    /// These methods include adding infrastructure configurations and applying mappings for <see cref="ValueObject"/> types.
+    /// </summary>
+    public static class ModelBuilderExtensions
     {
-        foreach (var assembly in assemblies)
+        /// <summary>
+        /// Applies infrastructure-specific configurations from the specified assemblies and adds transactional outbox entities.
+        /// </summary>
+        /// <param name="modelBuilder">The <see cref="ModelBuilder"/> instance to apply configurations to.</param>
+        /// <param name="assemblies">The assemblies that contain the configuration classes.</param>
+        public static void AddInfrastructure(this ModelBuilder modelBuilder, params Assembly[] assemblies)
         {
-            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            foreach (var assembly in assemblies)
+            {
+                modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            }
+
+            modelBuilder.ApplyValueObjectMappings();
+            modelBuilder.AddTransactionalOutboxEntities();
         }
 
-        modelBuilder.AddTransactionalOutboxEntities();
+        /// <summary>
+        /// Automatically applies mappings for <see cref="ValueObject"/> properties in entities.
+        /// This method configures each <see cref="ValueObject"/> as an owned type in the database using <see cref="OwnsOne"/>.
+        /// </summary>
+        /// <param name="modelBuilder">The <see cref="ModelBuilder"/> instance to apply the mappings to.</param>
+        private static void ApplyValueObjectMappings(this ModelBuilder modelBuilder)
+        {
+            var entities = modelBuilder.Model.GetEntityTypes()
+                .Where(e => e.ClrType?.IsClass == true);
+
+            foreach (IMutableEntityType entity in entities)
+            {
+                Type entityClrType = entity.ClrType;
+
+                // Get properties that are ValueObjects
+                var valueObjectProperties = entityClrType
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => typeof(ValueObject).IsAssignableFrom(p.PropertyType));
+
+                foreach (PropertyInfo prop in valueObjectProperties)
+                {
+                    // Dynamically apply OwnsOne configuration to owned value object properties
+                    var entityBuilderGeneric = typeof(ModelBuilder)
+                        .GetMethod(nameof(ModelBuilder.Entity), 1, Type.EmptyTypes)?
+                        .MakeGenericMethod(entityClrType)
+                        .Invoke(modelBuilder, null);
+
+                    MethodInfo? ownsOneMethod = entityBuilderGeneric?.GetType()
+                        .GetMethods()
+                        .FirstOrDefault(m => m.Name == nameof(EntityTypeBuilder<object>.OwnsOne)
+                                             && m.GetParameters().Length == 2)?
+                        .MakeGenericMethod(prop.PropertyType);
+
+                    if (ownsOneMethod != null)
+                    {
+                        // Create a lambda expression to map the property
+                        var parameter = Expression.Parameter(entityClrType, "e");
+                        var property = Expression.Property(parameter, prop);
+                        var lambda = Expression.Lambda(property, parameter);
+
+                        // Apply the OwnsOne method dynamically
+                        ownsOneMethod.Invoke(entityBuilderGeneric, new object[]
+                        {
+                            lambda, null!
+                        });
+                    }
+                }
+            }
+        }
     }
 }
